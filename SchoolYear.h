b5e85cd43a492559ce35fa5128cfcfd9c15e1117
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-03-08 09:14:38 +0700

    Update SchoolYear.h

--- SchoolYear.h
+++ SchoolYear.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include "List.h"
 using namespace std;
 
 struct Classes
@@ -13,12 +14,24 @@ struct Classes
 	*/
 	int ClassNo;
 };
-
+/*
 struct SchoolYear
 {
 	int StartYear;
 	Classes CurrentClasses;
 	SchoolYear* sNext;
+};
+*/
+struct InitYear
+{
+	int StartYear;
+	ClassList* classes;
+};
+
+struct YearList
+{
+	InitYear year;
+	YearList* next;
 };
 
 void CreateYearInfo(SchoolYear* CurY);
