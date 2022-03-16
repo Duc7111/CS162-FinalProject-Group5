385306ea36b22e7e33d4748b8ae9f5c6e2e1592d
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-03-16 13:42:50 +0700

    just a minor change
    cho tui hỏi cái (bool se[3];) là gì?

--- SchoolYear.h
+++ SchoolYear.h
@@ -2,12 +2,15 @@
 #include "AS.h"
 #include "Class.h"
 #include "semester.h"
+#include "course.h"
 struct schoolyear
 {
 	//data
+	int year;
 	char* name;
-	bool se[3];
+	bool se[3]; //se là gì?
 	semester* selist;
+	
 	//listpointer
 	schoolyear* next;
 
