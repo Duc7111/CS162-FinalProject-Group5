043dbdacfd05ef6e67d8e2f9efd001cdbb86b9c3
VThanhPhuc <99302070+VThanhPhuc@users.noreply.github.com>
2022-03-16 18:06:28 +0700

    Update SchoolYear.h

--- SchoolYear.h
+++ SchoolYear.h
@@ -10,14 +10,8 @@ struct schoolyear
 	char* name;
 	bool se[3]; //se là gì?
 	semester* selist;
-	
+
 	//listpointer
 	schoolyear* next;
 
-<<<<<<< HEAD
-	schoolyear();
 };
-=======
-	schoolyear() : name(nullptr), se({false, false, false}), selist(nullptr), next(nullptr){};
-};
->>>>>>> 385306ea36b22e7e33d4748b8ae9f5c6e2e1592d
