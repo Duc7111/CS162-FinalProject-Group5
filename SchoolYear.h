ee40a2dbb6266c360ba9a9941c91403a51c8d59e
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-03-16 22:47:08 +0700

    Update SchoolYear.h
    1 node school year sẽ chứa data bao gồm: năm học của khóa học, tên lớp -> tên lớp sẽ là 1 list chứa tên lớp (struct classname), Đức giúp tui chỉnh sửa cho phù hợp kiểu data ông nha!

--- SchoolYear.h
+++ SchoolYear.h
@@ -1,4 +1,5 @@
 #pragma once
+/*
 #include "AS.h"
 #include "Class.h"
 #include "semester.h"
@@ -12,4 +13,34 @@ struct schoolyear
 	schoolyear* next;
 
 	schoolyear();
+};
+*/
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+#include "AS.h"
+//#include "Screen.h"
+//#include "console.h"
+
+/*
+#include "Class.h"
+#include "semester.h"
+#include "student.h"
+#include "course.h"
+*/
+
+struct classname
+{
+	string cname = NULL;
+	classname* next;
+};
+
+struct schoolyear
+{
+	int year;
+	classname* newclass = NULL;
+	//semester* selist;
+	schoolyear* next;
 };
