2b3a3d66ca6dfa3689503d55118f69ad606d322b
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-03-17 15:33:27 +0700

    uses Class.h instead of a "name" string

--- SchoolYear.h
+++ SchoolYear.h
@@ -1,49 +1,27 @@
 #pragma once
+#include<iostream>
+#include<cstring>
+using namespace std; 
 
 #include "AS.h"
 #include "Class.h"
 #include "semester.h"
 
+//#include "Screen.h"
+//#include "console.h"
+
 struct schoolyear
 {
 	//data
-	string name;
+	Class* newclass;
 	semester* selist;
+	
 	//listpointer
 	schoolyear* next;
 
 	schoolyear();
 };
 
-#include<iostream>
-#include<cstring>
-using namespace std;
-
-#include "AS.h"
-//#include "Screen.h"
-//#include "console.h"
-
-/*
-#include "Class.h"
-#include "semester.h"
-#include "student.h"
-#include "course.h"
-*/
-
-struct classname
-{
-	string cname = NULL;
-	classname* next;
-};
-
-struct schoolyear
-{
-	int year;
-	classname* newclass = NULL;
-	//semester* selist;
-	schoolyear* next;
-};
-
 void CreateNY(schoolyear*& y, int data);
 void CreateNC(schoolyear*& y, int CurY);
-void CreateGeneralInfo(schoolyear*& y);
+void CreateGeneralInfo(schoolyear*& year);
