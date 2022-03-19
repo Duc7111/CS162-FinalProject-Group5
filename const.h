858edcc2286540437b7f187b28c9359032f06a25
VThanhPhuc <99302070+VThanhPhuc@users.noreply.github.com>
2022-03-19 09:08:34 +0700

    update

--- const.h
+++ const.h
@@ -7,7 +7,7 @@ const int ml[12] = {31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 void dataBase()
 {
-    mkdir("data");
-    mkdir("data\\schoolyears");
-    mkdir("data\\classes");
+    _mkdir("data");
+    _mkdir("data\\schoolyears");
+    _mkdir("data\\classes");
 }
