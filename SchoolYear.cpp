c3ab5af8088e4333ad6cf853fd67aa85f1653666
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-03-26 00:20:19 +0700

    Update SchoolYear.cpp

--- SchoolYear.cpp
+++ SchoolYear.cpp
@@ -52,7 +52,7 @@ bool checkSchoolYear(schoolyear& sy)
     return false;
 }
 
-void CreateGeneralInfo(schoolyear* &year)
+void CreateGeneralInfo(schoolyear& year)
 {
     //schoolyear* year = new schoolyear();
     cout << "1. Create a new school year." << endl;
@@ -65,44 +65,41 @@ void CreateGeneralInfo(schoolyear* &year)
 
     if (choice_1 == 1)
     {
-        int new_year;
-        cout << "Please enter the first year of the school year: ";
-        cin >> new_year;
+        string new_year;
+        cout << "Please enter the school year (EX: 2000-2001): ";
+        cin.ignore(1000, '\n');
+    	getline(cin, new_year);
         system("cls");
 
-        cout << "Your new school year is " << new_year << "-" << new_year + 1 << endl;
+        cout << "Your new school year is " << new_year << endl;
         cout << "Confirm creating this new school year?" << endl;
         cout << "1. Yes                           2. No" << endl;
         cout << "Your choice: ";
-        int choice_2; cin >> choice_2;
+        int choice_2; 
+	cin >> choice_2;
         system("cls");
-
-        //if (choice_2 == 1) CreateNY(*year, new_year);
+	    
+        if (choice_2 == 1) CreateNY(year, new_year);
     }
-    if (choice_1 == 2)
-    {
-        cout << "Please enter class's school year: ";
-        int new_year;
-        cin >> new_year;
-        //CreateNC(*year, new_year);
-    }
+    if (choice_1 == 2) CreateNC();
 }
 
-/*
 void CreateNY(schoolyear& y, string name) {
-    if (CheckDuplicateYear(y))
-    {
-        cout << "This school year has already existed!";
+    if (checkSchoolYear(y)) {
+        cout << "This school year has already existed.";
         return;
     }
-    y.name = name;
-    cout << "New school year is created!";
+    else {
+        y.save2File();
+        cout << "New school year has been created successfully" << endl;
+        cout << "You can now process this school year information, starting with semesters." << endl;
+        cout << "1. Yes, proceed to continue with semesters.            2. No, maybe later." << endl;
+        cout << "Your choice: ";
+        int choice;
+        cin >> choice;
+        //if (choice == 1) SemesterExecution(y);
+    }
 }
-void CreateNC(Class& y, string CurY) {
-    cout << "please enter the class name: ";
-    cin.ignore(1000, '\n');
-    getline(cin, y.name);
-    cout << "new class is created!";
-	return;
-}
-*/
+
+
+
