fa545440ca843271856178ca95a7d0a4c31d9ad3
John Wich <phantrungduc2003@gmail.com>
2022-04-14 15:24:46 +0700

    minor changes

--- course.h
+++ course.h
@@ -18,7 +18,7 @@ struct course
     int s[2]; //MON = 0, TUE = 1, ..., S1 = 1, S2 = 2, ... || s[x] = day*7 + S;
     int ms;
     int cur;
-    list<int[5]>* stlist; // 0: ID, 1: midterm, 2: final, 3: other, 4: total
+    list<float[5]>* stlist; // 0: ID, 1: midterm, 2: final, 3: other, 4: total
     
     course();
     course(const course& co);
