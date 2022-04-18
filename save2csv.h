d87c85aa485fdca5178b200c6885d71888991d2a
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-04-19 00:17:36 +0700

    Update save2csv.h

--- save2csv.h
+++ save2csv.h
@@ -6,7 +6,7 @@
 #include "console.h"
 #include <fstream>
 using namespace std;
-
+/*
 struct CourseSB {
     int StuNo;
     string StuID;
@@ -28,7 +28,8 @@ struct CourseSB {
         return c;
     }
 };
-
+*/
 void ExportStudentName2CSV(string dir);
-void ExportScoreBoard2CSV(string dir, list<CourseSB>*& ScoreBoard);
-void Import_StudentInfo_ScoreBoard(list<CourseSB>*& ScoreBoard);
+void ExportScoreBoard2CSV(string dir, course CurCourse);
+void ImportScoreBoard(string dir, course CurCourse);
+void ViewCourseScoreBoard(string dir, course CurCourse);
