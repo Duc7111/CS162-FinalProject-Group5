7d6a1104fd78b1d5cbd0e85f44f7c113c6253049
hikki0901 <95176051+hikki0901@users.noreply.github.com>
2022-04-24 20:55:40 +0700

    small fix

--- save2csv.h
+++ save2csv.h
@@ -6,30 +6,10 @@
 #include "console.h"
 #include <fstream>
 using namespace std;
-/*
-struct CourseSB {
-    int StuNo;
-    string StuID;
-    string FullName;
-    
-    double Midterm;
-    double Final;
-    double Total;
-    
-    list<double>* OtherMark;
 
-    int OM_AmountCount() {
-        int c = 0;
-        list<double>* tmp = OtherMark;
-        while (tmp != NULL) {
-            ++c;
-            tmp = tmp->next;
-        }
-        return c;
-    }
-};
-*/
 void ExportStudentName2CSV(string dir);
 void ExportScoreBoard2CSV(string dir, course CurCourse);
 void ImportScoreBoard(string dir, course CurCourse);
 void ViewCourseScoreBoard(string dir, course CurCourse);
+
+bool isNum(string s);
